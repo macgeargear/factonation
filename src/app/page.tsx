620eb6e3588d4eca7a563f803684096b7928d5e7
Gear <84141000+macgeargear@users.noreply.github.com>
2023-08-02 00:55:04 +0700

    chore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
 "use client";
-import { FC, useEffect, useState } from "react";
-import { IBM_Plex_Sans_Thai } from "next/font/google";
-import Herosection from "@/components/homePage/Herosection";
-import Problemsection from "@/components/homePage/Problemsection";
-import Benefitsection from "@/components/homePage/Benefitsection";
-import Reviewsection from "@/components/homePage/Reviewsection";
-import Questionsection from "@/components/homePage/Questionsection";
-import Lastsection from "@/components/homePage/Lastsection";
-import Footer from "@/components/homePage/Foooter";
-import { cn } from "@/lib/utils";
-import Navbar from "@/components/Navbar";
-import Pricesection from "@/components/homePage/Pricesection";
-import Lottie from "lottie-react";
-import Loading from "@/components/homePage/Loading.json";
-import Head from "next/head";
+
 import { useAuthContext } from "@/contexts/authContext";
+import Lottie from "lottie-react";
+import { FC, useEffect, useState } from "react";
+import Loading from "../components/homePage/Loading.json";
+import Navbar from "@/components/Navbar";
+import Herosection from "@/components/homePage/HeroSection";
+import Problemsection from "@/components/homePage/ProblemSection";
+import Benefitsection from "@/components/homePage/BenefitSection";
+import Reviewsection from "@/components/homePage/ReviewSection";
+import Pricesection from "@/components/homePage/PriceSection";
+import Questionsection from "@/components/homePage/QuestionSection";
+import Lastsection from "@/components/homePage/LastSection";
+import Footer from "@/components/homePage/Foooter";
 
 interface pageProps {}
 const Homepage: FC = ({}) => {
