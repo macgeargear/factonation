e0d5a76bd6a2da241c5d6ac983daa36da76c09f4
Gear <84141000+macgeargear@users.noreply.github.com>
2023-08-02 10:30:10 +0700

    fix: input

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -48,6 +48,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
     if (page === UserAuthFormPage.LogIn) {
       try {
         await login(usernameInput, passwordInput);
+        router.push("/");
       } catch (err) {
         console.log(err);
       }
@@ -123,7 +124,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
                   type="email"
                   autoComplete="email"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+                  className="px-2 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
                 />
               </div>
 
